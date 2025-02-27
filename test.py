db29d31e3f3fe63d55003f17195b9736833eebf0
ashutosh_j <ashutosh.j001@gmail.com>
2025-02-27 13:46:36 +0530

    update pipeline

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from fun import *
 class TestMathFunctions(unittest.TestCase):
 
     def test_factorial(self):
-        self.assertEqual(factorial(5), 12)
+        self.assertEqual(factorial(5), 120)
         self.assertEqual(factorial(0), 1)
         self.assertEqual(factorial(1), 1)
         with self.assertRaises(ValueError):
